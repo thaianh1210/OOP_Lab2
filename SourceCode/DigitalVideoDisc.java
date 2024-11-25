aeccacaf854d1c6fc33bde2128bd183e70db980c
thaianh1210 <thaianh102004@gmail.com>
2024-11-26 01:22:50 +0700

    Lab 03

--- SourceCode/DigitalVideoDisc.java
+++ SourceCode/DigitalVideoDisc.java
@@ -9,74 +9,73 @@
  */
 public class DigitalVideoDisc {
 
-        public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-            this.title = title;
-            this.category = category;
-            this.director = director;
-            this.length = length;
-            this.cost = cost;
-        }
-
-        public DigitalVideoDisc(String title, String category, String director, float cost) {
-            this.title = title;
-            this.category = category;
-            this.director = director;
-            this.cost = cost;
-        }
-
-        public DigitalVideoDisc(String title, String category, float cost) {
-            this.title = title;
-            this.category = category;
-            this.cost = cost;
-        }
-
-        public DigitalVideoDisc(String title) {
-            this.title = title;
-        }
-
-
-    public String getTitle() {
-        return title;
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
     }
 
-    public String getCategory() {
-        return category;
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.cost = cost;
     }
 
-    public String getDirector() {
-        return director;
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
     }
 
-    public int getLength() {
-        return length;
-    }
-
-    public float getCost() {
-        return cost;
-    }
-    public void setTitle(String title) {
+    public DigitalVideoDisc(String title) {
         this.title = title;
     }
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public void setLength(int length) {
-        this.length = length;
-    }
-
-    public void setCost(double cost) {
-        this.cost = (float) cost;
-    }
-    private String title;
-    private String category;
-    private String director;
-    private int length;
-    private float cost;
-    }
 
 
+public String getTitle() {
+    return title;
+}
+
+public String getCategory() {
+    return category;
+}
+
+public String getDirector() {
+    return director;
+}
+
+public int getLength() {
+    return length;
+}
+
+public float getCost() {
+    return cost;
+}
+public void setTitle(String title) {
+    this.title = title;
+}
+
+public void setCategory(String category) {
+    this.category = category;
+}
+
+public void setDirector(String director) {
+    this.director = director;
+}
+
+public void setLength(int length) {
+    this.length = length;
+}
+
+public void setCost(double cost) {
+    this.cost = (float) cost;
+}
+private String title;
+private String category;
+private String director;
+private int length;
+private float cost;
+}
