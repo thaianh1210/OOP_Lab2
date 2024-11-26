16ac1f1625f00fa4e4553e461cf2a0bf7fd31146
thaianh1210 <thaianh102004@gmail.com>
2024-11-26 07:10:45 +0700

    Lab03

--- SourceCode/DigitalVideoDisc.java
+++ SourceCode/DigitalVideoDisc.java
@@ -8,33 +8,59 @@
  * @author Admin
  */
 public class DigitalVideoDisc {
+    // Class attribute (biến thành viên lớp) (Lab03 Bài 5)
+    private static int nbDigitalVideoDiscs = 0;
 
-        public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-            this.title = title;
-            this.category = category;
-            this.director = director;
-            this.length = length;
-            this.cost = cost;
-        }
+    // Instance attributes (biến thành viên đối tượng)
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+    private int id; // Instance attribute to hold unique ID
 
-        public DigitalVideoDisc(String title, String category, String director, float cost) {
-            this.title = title;
-            this.category = category;
-            this.director = director;
-            this.cost = cost;
-        }
+    // Constructor method
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+        assignId(); // Assign a unique ID
+    }
 
-        public DigitalVideoDisc(String title, String category, float cost) {
-            this.title = title;
-            this.category = category;
-            this.cost = cost;
-        }
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.cost = cost;
+        assignId(); // Assign a unique ID
+    }
 
-        public DigitalVideoDisc(String title) {
-            this.title = title;
-        }
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        assignId(); // Assign a unique ID
+    }
 
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+        assignId(); // Assign a unique ID
+    }
 
+    // Method to assign a unique ID to the DVD
+    private void assignId() {
+        nbDigitalVideoDiscs++; // Increment the static counter
+        this.id = nbDigitalVideoDiscs; // Assign the current count as the ID
+    }
+
+    // Getter for ID
+    public int getId() {
+        return id;
+    }
+
+    // Getters and Setters
     public String getTitle() {
         return title;
     }
@@ -54,11 +80,24 @@ public class DigitalVideoDisc {
     public float getCost() {
         return cost;
     }
-    private String title;
-    private String category;
-    private String director;
-    private int length;
-    private float cost;
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
+    public void setCategory(String category) {
+        this.category = category;
+    }
 
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setCost(double cost) {
+        this.cost = (float) cost;
+    }
+}
